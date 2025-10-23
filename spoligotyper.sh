4c4b46bcea4b4289bace08bed0a823360f06080a
Marcopterix <91996285+marco315176@users.noreply.github.com>
2025-10-23 11:17:46 -0600

    Change directory paths in spoligotyper.sh
    Updated directory paths to point to the user's home directory.

--- spoligotyper.sh
+++ spoligotyper.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-cd /home/secuenciacion_cenasa/MBovis_seq
+cd /home/user/MBovis_seq
 
 for R1 in *_R1_*fastq.gz; do
     R2=${R1/_R1_/_R2_}
     ID=$(basename ${R1} | cut -d '_' -f '1')
 
-spoligotyper.py -r1 ${R1} -r2 ${R2} -m 5 -o /home/secuenciacion_cenasa/MBovis_seq/Spoligotyper
+spoligotyper.py -r1 ${R1} -r2 ${R2} -m 5 -o /home/user/MBovis_seq/Spoligotyper
 
 done
 
-cd /home/secuenciacion_cenasa/MBovis_seq/Spoligotyper
+cd /home/user/MBovis_seq/Spoligotyper
 
 cat ./*.txt >> ./Spoligotyper_prediction.tsv
 
-rm /home/secuenciacion_cenasa/MBovis_seq/Spoligotyper/*.txt
+rm /home/user/MBovis_seq/Spoligotyper/*.txt
